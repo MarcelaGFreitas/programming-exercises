d21457fd1de17276a2247bce123d3fdcd94a5a16
Marcela Freitas <gfreitas.marcela@gmail.com>
2026-02-27 16:48:33 -0300

    refactor: implement manual bubble sort algorithm to sort array

--- java/15_array/src/SortArray.java
+++ java/15_array/src/SortArray.java
@@ -21,12 +21,12 @@ public class SortArray {
         System.out.print("Original array: ");
         for(int i=0; i<5; i++) System.out.print(numbers[i] + " ");
 
-        for(int i=0; i<5; i++){
-            for(int j=0; j<i; j++){
-                if(numbers[i] < numbers[j]){
+        for(int i=0; i<numbers.length-1; i++){
+            for(int j=0; j<numbers.length-1-i; j++){
+                if(numbers[j] > numbers[j+1]){
                     int temporaryVar = numbers[j];
-                    numbers[j] = numbers[i];
-                    numbers[i] = temporaryVar;
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temporaryVar;
                 }
             }
         }
