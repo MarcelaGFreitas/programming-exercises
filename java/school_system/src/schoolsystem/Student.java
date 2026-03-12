a83f19a31fa903af40c1616de249b5105d41cc94
Marcela Freitas <gfreitas.marcela@gmail.com>
2026-03-12 16:01:37 -0300

    Organize project structure and add schoolsystem package

---
+++ java/school_system/src/schoolsystem/Student.java
@@ -0,0 +1,12 @@
+public class Student {
+    
+    // Attributes
+    String name;
+    int registrationNumber;
+
+    // Constructor
+    public Student(String name, int registrationNumber){
+        this.name = name;
+        this.registrationNumber = registrationNumber;
+    }
+}
